5f5fc57063fb82a17b532be23a1e38eedeafc7d1
Documenter.jl <documenter@juliadocs.github.io>
2023-06-14 09:00:50 +0000

    build based on 9aeed25

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CorpusCleanerForTWLaws","category":"page"},{"location":"#CorpusCleanerForTWLaws","page":"Home","title":"CorpusCleanerForTWLaws","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CorpusCleanerForTWLaws.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CorpusCleanerForTWLaws]","category":"page"}]
+}
