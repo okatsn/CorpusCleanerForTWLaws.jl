099b42f8d6d11557f0b634b62fe74be1cda43f03
okatsn <okatsn@gmail.com>
2023-06-15 15:59:00 +0800

    Clean for CorpusJudicalYuan; Tests passed

--- src/cleancorpus.jl
+++ src/cleancorpus.jl
@@ -1,6 +1,35 @@
+expr_qncolon = r"\AQ\d+\s*\W\s*"
+# Shortnotes:
+# - r"\W" matches "：" or "？"
+# - r"\w+" matches "中文"
+expr_manylnbr = r"(\r|\n)+(?=\n)"
+expr_tab = r"\t"
+expr_1stlnbr = r"\A(\n|\r)+"
+expr_endlnbr = r"(\n|\r)+\Z"
+
+"""
+`replacex(content, p...::Pair)` replace `content` with pairs of strings.
+Currently, `replace` supports only one string substitution (i.e., `replace(c, "hello" => "world")`)
+
+"""
+function replacex(content, prs...; kwargs...)
+    for p in prs
+        content = replace(content, p; kwargs...)
+    end
+    return content
+end
+
+
 function clean!(cp::CorpusJudicalYuan)
-    cp.question = replace(cp.question, r"\AQ\d+\s*\W\s*" => "")
-    # Shortnotes:
-    # r"\W" matches "：" or "？"
-    # r"\w+" matches "中文"
+    cp.question = replace(cp.question, expr_qncolon => "")
+    answer = replacex(cp.answer,
+        r"\r" => "", # remove all carridge return
+        expr_tab => "",
+        Regex(cp.question) => "",
+        expr_manylnbr => "",
+        expr_1stlnbr => "",
+        expr_endlnbr => ""
+    )
+    cp.answer = answer
+
 end
--- test/cleancorpus.jl
+++ test/cleancorpus.jl
@@ -1,3 +1,11 @@
+@testset "test regex for clean" begin
+    @test match(CorpusCleanerForTWLaws.expr_qncolon, "Q12 ： 哈囉隨便拉").match == "Q12 ： "
+    @test isnothing(match(CorpusCleanerForTWLaws.expr_qncolon, "FAQ12：J個不是目標"))
+    @test match(CorpusCleanerForTWLaws.expr_manylnbr, "我想提起行政訴訟，但不知道應以哪個機關為被告？\r\n\r\n提起行政訴訟").match == "\r\n\r"
+    @test match(CorpusCleanerForTWLaws.expr_manylnbr, "被告也會有所不同。\r\n\r\n\r\n\r\n撤銷訴訟或課予義務訴").match == "\r\n\r\n\r\n\r"
+    @test match(CorpusCleanerForTWLaws.expr_manylnbr, "被告也會有所不同。\n\n\n\n撤銷訴訟或課予義務訴").match == "\n\n\n"
+end
+
 @testset "cleancorpus.jl" begin
     # Test question cleaning
     cp = CorpusJudicalYuan("Q38 : 言詞辯論一定要請律師嗎？我沒有錢請律師怎麼辦？", "")
@@ -12,4 +20,7 @@
     clean!(cp)
     @test cp.question == "職務法庭是否採行公開審理程序？"
 
+    cp = CorpusJudicalYuan("", "被告也會有所不同。\r\n\r\n\r\n\t\r\n\t撤銷訴訟或課予義務訴")
+    clean!(cp)
+    @test cp.answer == "被告也會有所不同。\n撤銷訴訟或課予義務訴"
 end
