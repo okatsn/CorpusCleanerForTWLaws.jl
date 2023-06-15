70ee2cc7e970401f4228911fd057266ee19d8b4c
okatsn <okatsn@gmail.com>
2023-06-15 16:06:42 +0800

    Dict for c <: CorpusType

--- src/corpustype.jl
+++ src/corpustype.jl
@@ -8,3 +8,13 @@ end
 function CorpusJudicalYuan(d::Dict)
     CorpusJudicalYuan(d["question"], d["answer"])
 end
+
+
+function CorpusCleanerForTWLaws.Dict(c::CorpusType)
+    ckeys = fieldnames(typeof(c))
+    d = Dict()
+    for k in ckeys
+        d[k] = getproperty(c, k)
+    end
+    return d
+end
